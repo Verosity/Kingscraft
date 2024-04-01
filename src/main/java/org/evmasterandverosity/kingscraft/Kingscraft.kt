dec75f01435db32f226e67f23a18f81b462f9c24
Verosity <165721188+Verosity@users.noreply.github.com>
2024-04-01 14:08:34 -0700

    Initial commit

---
+++ src/main/java/org/evmasterandverosity/kingscraft/Kingscraft.kt
@@ -0,0 +1,16 @@
+package org.evmasterandverosity.kingscraft
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Kingscraft : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+        logger.info("Kingscraft Plugin Starting")
+        logger.info("Kingscraft Plugin Enabled")
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+        logger.info("Kingscraft Plugin Disabled")
+    }
+}
